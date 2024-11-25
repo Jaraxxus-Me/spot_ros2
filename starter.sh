aa0b58e2d87e4f7beda925f1bec7820898f5d8d1
Bowen Li <bowenli2@andrew.cmu.edu>
2024-11-24 21:22:07 -0500

    del

--- starter.sh
+++
@@ -1,8 +0,0 @@
-echo "Checking WIFI Connection..."
-echo ping 192.168.80.3
-python3 -m bosdyn.client 192.168.80.3 id
-
-export BOSDYN_CLIENT_USERNAME=???
-export BOSDYN_CLIENT_PASSWORD=???
-
-python3 src/spot_ros2/hello_spot.py 192.168.80.3
