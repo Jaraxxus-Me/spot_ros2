3b709e2e7584ffa0be3811b3c18648305bf5ffbc
Bowen Li <bowenli2@andrew.cmu.edu>
2024-11-24 21:20:02 -0500

    no leak of pwd

--- starter.sh
+++ starter.sh
@@ -2,7 +2,7 @@ echo "Checking WIFI Connection..."
 echo ping 192.168.80.3
 python3 -m bosdyn.client 192.168.80.3 id
 
-export BOSDYN_CLIENT_USERNAME=admin
-export BOSDYN_CLIENT_PASSWORD=jtidtt7fbxh5
+export BOSDYN_CLIENT_USERNAME=???
+export BOSDYN_CLIENT_PASSWORD=???
 
 python3 src/spot_ros2/hello_spot.py 192.168.80.3
